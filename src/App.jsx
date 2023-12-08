c7729ded54c16f59371e7e9aea910565a5a68163
Mark.Lannen <mark.lannen@mso.umt.edu>
2023-12-08 15:29:05 -0700

    add tailwinds package. Creat button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import Button from './Button';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button success primary rounded outline>
+          Click Me!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          Buy Now
+        </Button>
+      </div>
+      <div>
+        <Button warning>See Deal</Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads
+        </Button>
+      </div>
+      <div>
+        <Button secondary rounded>
+          Something else
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
