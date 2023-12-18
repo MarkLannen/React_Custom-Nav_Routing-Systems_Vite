16fb0ae1b97fb232b6a36c49445ee72e12053b3d
Mark.Lannen <mark.lannen@mso.umt.edu>
2023-12-18 15:50:18 -0700

    add functionality and styling to accordions

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import Accordion from './assets/components/Accordion';
 
 function App() {
-  const itmes = [
+  const items = [
     {
+      id: 'skdfj',
       label: 'Can I use React on a Project?',
       content:
         ' You can use React on any project.You can use React on any project. You can use React on any project.',
     },
     {
+      id: '234j',
       label: 'Can I use Javascript on a Project?',
       content:
         'You can use React on any project. You can use React on any project.You can use React on any project.',
     },
     {
+      id: 'x,cmvn',
       label: 'Can I use CSS on a Project?',
       content:
         'You can use React on any project.You can use React on any project.You can use React on any project.',
