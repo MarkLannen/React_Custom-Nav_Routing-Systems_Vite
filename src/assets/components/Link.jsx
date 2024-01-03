0ff357b8667deb0100768782dba1270f50202688
Mark.Lannen <mark.lannen@mso.umt.edu>
2024-01-03 09:21:00 -0700

    add functionality to Link component that enables user to open new tab with command or control click.

--- src/assets/components/Link.jsx
+++ src/assets/components/Link.jsx
@@ -5,6 +5,9 @@ import NavigationContext from '../../context/navigation';
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
   const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
     navigate(to);
   };
