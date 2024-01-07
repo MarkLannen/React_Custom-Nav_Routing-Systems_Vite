2fbb53cc953b6b2810b58840663422cadb9531c0
Mark Lannen <marklannendev@gmail.com>
2024-01-06 19:15:34 -0700

    add Sidebar component and update styling in App.jsx to display Sidebar to the side

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import Link from './assets/components/Link';
 import Route from './assets/components/Route';
+import Sidebar from './assets/components/Sidebar';
 import AccordionPage from './assets/pages/AccordionPage';
 import DropdownPage from './assets/pages/DropdownPage';
+import ButtonPage from './assets/pages/ButtonPage';
 
 function App() {
-  return (
-    <div>
-      <Link to='/accordion'>Go to accordion</Link>
-      <Link to='/dropdown'>Go to dropdown</Link>
-      <div>
-        <Route path='/accordion'>
-          <AccordionPage />
-        </Route>
-        <Route path='/dropdown'>
-          <DropdownPage />
-        </Route>
-      </div>
-    </div>
-  );
+	return (
+		<div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+			<Sidebar />
+			<div className="col-span-5">
+				<Route path="/accordion">
+					<AccordionPage />
+				</Route>
+				<Route path="/">
+					<DropdownPage />
+				</Route>
+				<Route path="/buttons">
+					<ButtonPage />
+				</Route>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/context/navigation.jsx
+++ src/context/navigation.jsx
@@ -3,29 +3,28 @@ import { createContext, useState, useEffect } from 'react';
 const NavigationContext = createContext();
 
 const NavigationProvider = ({ children }) => {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+	const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
-  useEffect(() => {
-    const handler = () => {
-      setCurrentPath(window.location.pathname);
-    };
-    window.addEventListener('popstate', handler);
-    return () => {
-      window.removeEventListener('popstate', handler);
-    };
-  }, []);
+	useEffect(() => {
+		const handler = () => {
+			setCurrentPath(window.location.pathname);
+		};
+		window.addEventListener('popstate', handler);
+		return () => {
+			window.removeEventListener('popstate', handler);
+		};
+	}, []);
 
-  const navigate = (to) => {
-    window.history.pushState({}, '', to);
-    setCurrentPath(to);
-  };
+	const navigate = (to) => {
+		window.history.pushState({}, '', to);
+		setCurrentPath(to);
+	};
 
-  return (
-    <NavigationContext.Provider value={{ currentPath, navigate }}>
-      {currentPath}
-      {children}
-    </NavigationContext.Provider>
-  );
+	return (
+		<NavigationContext.Provider value={{ currentPath, navigate }}>
+			{children}
+		</NavigationContext.Provider>
+	);
 };
 
 export { NavigationProvider };
