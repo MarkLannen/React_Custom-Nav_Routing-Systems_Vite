4b441ce9eaa283bc85602ea98c3bc475ebc19736
Mark.Lannen <mark.lannen@mso.umt.edu>
2024-01-08 15:51:45 -0700

    position text and button inside modal

--- src/assets/components/Modal.jsx
+++ src/assets/components/Modal.jsx
@@ -9,8 +9,8 @@ function Modal({ onClose, children, actionBar }) {
         className='absolute inset-0 bg-gray-300 opacity-80'
       ></div>
       <div className='absolute inset-40 p-10 bg-white h-fit'>
-        {children}
-        {actionBar}
+        <div className='flex flex-col justify-between h-full '>{children}</div>
+        <div className='flex justify-end'> {actionBar}</div>
       </div>
     </div>,
     document.querySelector('.modal-container')
