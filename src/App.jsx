9fe6c3e8f4264cbddfeed99e3710751e0d89566b
Mark.Lannen <mark.lannen@mso.umt.edu>
2024-01-08 14:38:42 -0700

    create Modal component and ModalPage component. Add ModalPage Route to App.jsx and Sidebar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,28 @@ import Sidebar from './assets/components/Sidebar';
 import AccordionPage from './assets/pages/AccordionPage';
 import DropdownPage from './assets/pages/DropdownPage';
 import ButtonPage from './assets/pages/ButtonPage';
+import ModalPage from './assets/pages/ModalPage';
 
 function App() {
-	return (
-		<div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
-			<Sidebar />
-			<div className="col-span-5">
-				<Route path="/accordion">
-					<AccordionPage />
-				</Route>
-				<Route path="/">
-					<DropdownPage />
-				</Route>
-				<Route path="/buttons">
-					<ButtonPage />
-				</Route>
-			</div>
-		</div>
-	);
+  return (
+    <div className='container mx-auto grid grid-cols-6 gap-4 mt-4'>
+      <Sidebar />
+      <div className='col-span-5'>
+        <Route path='/accordion'>
+          <AccordionPage />
+        </Route>
+        <Route path='/'>
+          <DropdownPage />
+        </Route>
+        <Route path='/buttons'>
+          <ButtonPage />
+        </Route>
+        <Route path='/modal'>
+          <ModalPage />
+        </Route>
+      </div>
+    </div>
+  );
 }
 
 export default App;
