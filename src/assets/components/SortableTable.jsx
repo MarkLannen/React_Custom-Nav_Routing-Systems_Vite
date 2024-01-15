fbce907766ddc40967fa02f083125801dd1eb7a2
Mark Lannen <marklannendev@gmail.com>
2024-01-15 09:26:12 -0700

    add State to SortableTable component to track how Table should be sorted - null, asc or desc.

--- src/assets/components/SortableTable.jsx
+++ src/assets/components/SortableTable.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 import Table from './Table';
 
 function SortableTable(props) {
+	const [sortOrder, setSortOder] = useState(null);
+	const [sortBy, setSortby] = useState(null);
 	const { config } = props;
+
+	const handleClick = (label) => {
+		if (sortOrder === null) {
+			setSortOder('asc');
+			setSortby(label);
+		} else if (sortOrder === 'asc') {
+			setSortOder('desc');
+			setSortby(label);
+		} else if (sortOrder === 'desc') {
+			setSortOder(null);
+			setSortby(null);
+		}
+	};
 
 	const updatedConfig = config.map((column) => {
 		if (!column.sortValue) {
@@ -11,11 +27,20 @@ function SortableTable(props) {
 
 		return {
 			...column,
-			header: () => <th>{column.label} IS SORTABLE</th>,
+			header: () => (
+				<th key={column.label} onClick={() => handleClick(column.label)}>
+					{column.label} IS SORTABLE
+				</th>
+			),
 		};
 	});
 
-	return <Table {...props} config={updatedConfig} />;
+	return (
+		<div>
+			{sortOrder} - {sortBy}
+			<Table {...props} config={updatedConfig} />
+		</div>
+	);
 }
 
 export default SortableTable;
